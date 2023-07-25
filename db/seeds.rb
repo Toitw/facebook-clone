ff0149aecdd2fd34cb3a8d4a698f2a79a0aa1b91
juroga_new <juanjrgast@gmail.com>
2023-07-25 17:55:44 +0200

    Populate seed, add name to User model and create Users controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#Create 5 random users
+User.create!(name: "John Doe", email: "john44@yahoo.es", password: "123456")
+User.create!(name: "Jane Doe", email: "jane3@myemail.com", password: "123456")
+User.create!(name: "John Smith", email: "smither@yahoo.com", password: "123456")
+User.create!(name: "Jane Smith", email: "jani@myemail.com", password: "123456")
+User.create!(name: "Olaya Rosell", email: "olayarosell@hotmail.com", password: "123456")
+User.create!(name: "Jesus Rodriguez", email: "jesusisto@gmail.com", password: "123456")
+User.create!(name: "Joya Area", email: "lajoya@gmail.com", password: "123456")
