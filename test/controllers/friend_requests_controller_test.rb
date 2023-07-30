4691c803a89ad6199eab94a218048bffd48cf1b6
juroga_new <juanjrgast@gmail.com>
2023-07-30 18:40:29 +0200

    Created tests for friend requests

--- test/controllers/friend_requests_controller_test.rb
+++ test/controllers/friend_requests_controller_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class FriendRequestsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one) 
+    sign_in @user
+  end
+
+  test "should get index" do
+    get friend_requests_path
+    assert_response :success
+  end
+
+  test "should create friend request" do
+    recipient = users(:two)
+    post friend_requests_path(recipient_id: recipient.id)
+    assert_response :redirect
+  end
+  
+
+  test "should update friend request" do
+    friend_request = friend_requests(:one) 
+    patch friend_request_path(friend_request)
+    assert_response :redirect
+  end
+  
+
+  test "should destroy friend request" do
+    friend_request = friend_requests(:one) # Assuming you have a fixture or factory for friend_requests
+    delete friend_request_path(friend_request)
+    assert_response :redirect
+  end
+  
 end
