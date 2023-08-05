8fc4d30aa1a16021e816863edd36eceb70cce8fc
juroga_new <juanjrgast@gmail.com>
2023-08-05 11:12:40 +0200

    Fixed Like so if the user already gave a like and click again, destroy action is called

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,20 @@ class LikesController < ApplicationController
 
     def create
         @post = Post.find(params[:like][:post_id])
-        @user = current_user
-        @like = @post.likes.build(user: current_user)
-        if @like.save
-            redirect_to posts_path
+        @like = @post.likes.find_by(user: current_user)
+      
+        if @like
+          # User already liked the post, so we destroy the like
+          @like.destroy
         else
-            render 'new'
+          # User has not liked the post yet, so we create a new like
+          @like = @post.likes.build(user: current_user)
+          @like.save
         end
+      
+        redirect_to posts_path
     end
+      
 
     def destroy
         @post = Post.find(params[:post_id])
