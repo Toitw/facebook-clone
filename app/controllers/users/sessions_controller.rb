433e0a19abc03e3a22829551523852b8e78bcf97
juroga_new <juanjrgast@gmail.com>
2023-08-13 14:31:45 +0200

    Configured User#sessions to send an email when signing up

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,9 +9,11 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super do |user|
+      UserMailer.with(user: user).welcome_email.deliver_later
+    end
+  end
 
   # DELETE /resource/sign_out
   def destroy
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,10 +1,26 @@
 require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
+
+  include Devise::Test::IntegrationHelpers
+
   test "welcome_email" do
     user = User.create(email: 'testemail@juroga.com', password: 'password', name: 'juroga')
     mail = UserMailer.with(user: user).welcome_email
     assert_equal "Welcome to Odin Facebook", mail.subject
     assert_equal [user.email], mail.to
+  end
+
+  test 'welcome email is sent' do
+    user = User.create(email: 'testemail2@juroga.com', password: 'password', name: 'juroga2')
+    email = UserMailer.with(user: user).welcome_email
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['odinfacebook@juroga.com'], email.from
+    assert_equal [user.email], email.to
+    assert_equal 'Welcome to Odin Facebook', email.subject
   end
 end
